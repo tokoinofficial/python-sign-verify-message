3bb59070ea2d3f6cd27ec028632494f68d36b22c
Trong Dinh <trongdth@gmail.com>
2023-04-18 10:28:30 +0700

    update code

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         '0x45f7967926e95fd161e56ed66b663c9114c5226f'), 4685]
     signature = sign(
         "0x16870e97136d178735994d9ed537742276db9f30d80b2b35a05a798895679049", types, values)
-    print(signature)
+    print(signature.hex())
 
     # verify message
     print(verify('0xfE91b1E07b93fdfae1E02A985266fA3414aB844A', types, values, signature))
